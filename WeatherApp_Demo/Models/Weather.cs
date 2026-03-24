d82986ef64bc30f3e5d9a08da0962a2c36a1572a
Minhlth <hieuminh20022005@gmail.com>
2026-03-25 00:49:00 +0700

    Add project files.

---
+++ WeatherApp_Demo/Models/Weather.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp_Demo.Models
+{
+    public class Weather
+    {
+        public int ID { get; set; }
+        public string LocationName { get; set; } = string.Empty;
+
+    }
+}
