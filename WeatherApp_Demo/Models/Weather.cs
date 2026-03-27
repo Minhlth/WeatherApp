120e5e4ce7f11cad1a9c5d7690c809f67bc200b4
Minhlth <hieuminh20022005@gmail.com>
2026-03-27 19:19:10 +0700

    update code

--- WeatherApp_Demo/Models/Weather.cs
+++ WeatherApp_Demo/Models/Weather.cs
@@ -8,6 +8,18 @@ namespace WeatherApp_Demo.Models
     {
         public int ID { get; set; }
         public string LocationName { get; set; } = string.Empty;
+        public double Temperature { get; set; }
+        public string Condination { get; set; } = string.Empty;
+        public int Humidity { get; set; }
+        public int UVIndex { get; set; }
+        public int AQI { get; set; }
 
+     }
+    public class DailyForecast
+    {
+        public string DayName { get; set; } = string.Empty; 
+        public double TempMin { get; set; }
+        public double TempMax { get; set; }
+        public string IconPath { get; set; } = string.Empty;
     }
 }
